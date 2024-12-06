c4adfce6834d8d2c0173b0b0f62889542915e618
Radoslav97 <69162297+Radoslav97@users.noreply.github.com>
2024-12-06 16:49:41 +0100

    Add files via upload

---
+++ microservice22/src/main/java/com/example/demo/Microservice22Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice22Application.class, args);
+		System.out.println("Application started!!");
+	}
+
+}
